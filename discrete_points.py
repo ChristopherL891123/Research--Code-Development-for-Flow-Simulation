1c3afef40f8ae68cb20cea7182af4d08c0a30c36
Lama, Christopher <lamac@stthom.edu>
2022-02-10 18:50:23 -0600

    added LU.py

--- discrete_points.py
+++ discrete_points.py
@@ -12,109 +12,103 @@ import time
 # if the matrix size changes, update the size of the dependent matrix, the first for loop, the size of the table.
 
 # --> Calculations <--
-num = []
-for i in range(100):
-    T1 =  time.perf_counter()
 
 
-    H = 1
-    L = 5
-    Delta_P = 8.0
-    Viscosity = 0.42
-    Delta_Y = (2 * H) / 4
-    V_max = (Delta_P * H ** 2) / (2 * Viscosity * L)
 
-    F_j = (2 * ((8 * 1) / (2 * 0.42 * 5)))
-    F_j *= 0.5 ** 2
+H = 1
+L = 5
+Delta_P = 8.0
+Viscosity = 0.42
+Delta_Y = (2 * H) / 4
+V_max = (Delta_P * H ** 2) / (2 * Viscosity * L)
 
-    V_exact_LIST = []
-    Y_j_LIST = []
+F_j = (2 * ((8 * 1) / (2 * 0.42 * 5)))
+F_j *= 0.5 ** 2
 
-    # calculate Y_j and V_exact and append it to their respective lists for data processing
-    for i in range(5):
-        Y_j = (-H + i * Delta_Y)
-        Y_j_LIST.append(Y_j)
-        V_exact_LIST.append(V_max * (1 - (Y_j / H) ** 2))
+V_exact_LIST = []
+Y_j_LIST = []
 
-    Y_j_LIST.insert(0, 'blank')  # for adding this to the table( 'blank' will not be displayed on the table )
+# calculate Y_j and V_exact and append it to their respective lists for data processing
+for i in range(5):
+    Y_j = (-H + i * Delta_Y)
+    Y_j_LIST.append(Y_j)
+    V_exact_LIST.append(V_max * (1 - (Y_j / H) ** 2))
 
-    # create matrix
-    Matrix = np.array([[2, -1, 0], [-1, 2, -1], [0, -1, 2]])
+Y_j_LIST.insert(0, 'blank')  # for adding this to the table( 'blank' will not be displayed on the table )
 
-    print("Matrix: \n", Matrix)
+# create matrix
+Matrix = np.array([[2, -1, 0], [-1, 2, -1], [0, -1, 2]])
 
-    # create dependent variable matrix
-    Dependent_Matrix = np.array([[F_j], [F_j], [F_j]])
+print("Matrix: \n", Matrix)
 
-    print("\nDependent Matrix: \n", Dependent_Matrix)
+# create dependent variable matrix
+Dependent_Matrix = np.array([[F_j], [F_j], [F_j]])
 
-    # solve the matrix using the solve function in Numpy library, this function returns another matrix(ndarray)
-    # where the solutions are stored. Does not show zeros.
-    #result_withNumPy = np.linalg.solve(Matrix, Dependent_Matrix)
+print("\nDependent Matrix: \n", Dependent_Matrix)
 
-    # verified my answer using SciPy library
-    result_withNumPy = np.linalg.solve(Matrix , Dependent_Matrix)
+# solve the matrix using the solve function in Numpy library, this function returns another matrix(ndarray)
+# where the solutions are stored. Does not show zeros.
+#result_withNumPy = np.linalg.solve(Matrix, Dependent_Matrix)
 
-
-    # --> Display results <--
+# verified my answer using SciPy library
+result_withNumPy = np.linalg.solve(Matrix , Dependent_Matrix)
 
 
-    #create table of size 6 rows, 3 columns. 1 row is for the header and 5 rows are for the results.
+# --> Display results <--
+
+
+#create table of size 6 rows, 3 columns. 1 row is for the header and 5 rows are for the results.
 
 
 
 
-    Table = [[0.0, 0.0, 0.0] for x in range(6)]
+Table = [[0.0, 0.0, 0.0] for i in range(6)]
 
 
-    # add the values of Y_j to the table
-    counter1 = 0  # used to reference the index of Y_j_LIST
-    for nested_lists in Table:
-        nested_lists[0] = Y_j_LIST[counter1]
-        counter1 += 1
+# add the values of Y_j to the table
+counter1 = 0  # used to reference the index of Y_j_LIST
+for nested_lists in Table:
+    nested_lists[0] = Y_j_LIST[counter1]
+    counter1 += 1
 
-    # add values of solutions to table
-    counter2 = 0
-    counter3 = 0  # since values of row 2 and last row will always be 0, we need to skip those rows. This is the purpose of this variable
-    for nested_lists2 in Table:
-        counter3 += 1
-        if counter3 <= 2:
-            continue
-        nested_lists2[1] = result_withNumPy[counter2][0]
-        counter2 += 1
+# add values of solutions to table
+counter2 = 0
+counter3 = 0  # since values of row 2 and last row will always be 0, we need to skip those rows. This is the purpose of this variable
+for nested_lists2 in Table:
+    counter3 += 1
+    if counter3 <= 2:
+        continue
+    nested_lists2[1] = result_withNumPy[counter2][0]
+    counter2 += 1
 
-        if counter2 == len(result_withNumPy):
-            break
+    if counter2 == len(result_withNumPy):
+        break
 
-    # add values of exact velocities to table
-    counter4 = 0
-    counter5 = 0  # since values of row 2 and last row will always be 0, we need to skip those rows. This is the purpose of this variable
-    for nested_lists3 in Table:
-        counter5 += 1
-        if counter5 <= 1:
-            continue
-        nested_lists3[2] = V_exact_LIST[counter4]
-        counter4 += 1
+# add values of exact velocities to table
+counter4 = 0
+counter5 = 0  # since values of row 2 and last row will always be 0, we need to skip those rows. This is the purpose of this variable
+for nested_lists3 in Table:
+    counter5 += 1
+    if counter5 <= 1:
+        continue
+    nested_lists3[2] = V_exact_LIST[counter4]
+    counter4 += 1
 
-        if counter4 == len(V_exact_LIST):
-            break
+    if counter4 == len(V_exact_LIST):
+        break
 
-    # assign values for the headers (first row)
-    Table[0][0] = "Y_j"
-    Table[0][1] = "Solution"
-    Table[0][2] = "V Exact"
+# assign values for the headers (first row)
+Table[0][0] = "Y_j"
+Table[0][1] = "Solution"
+Table[0][2] = "V Exact"
 
 
 
 
 
-    print("Table: \n", tb(Table, tablefmt='fancy_grid'),sep='')
-    #NOTE: use showindex=True to count how many rows there are in the table
+print("Table: \n", tb(Table, tablefmt='fancy_grid'),sep='')
+#NOTE: use showindex=True to count how many rows there are in the table
 
 
-    T2 = time.perf_counter()
 
-    FinalTime = T2 - T1
-    num.append(FinalTime)
 
-print(num)
