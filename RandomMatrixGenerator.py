25cdd787c49b1115528c12310de605d3f7d73090
Lama, Christopher <lamac@stthom.edu>
2022-03-02 11:14:23 -0600

    made changes to LU.py

--- RandomMatrixGenerator.py
+++ RandomMatrixGenerator.py
@@ -8,9 +8,21 @@ def RAND(size):
         R.append([])
         for j in range(size):
             R[i].append(random.randrange(-20,20))
+        R[i][i] = 2
     return R
 
 #test
-a = RAND(3)
+a = RAND(9)
 for i in range(len(a)):
     print(a[i])
+
+def diagonalCheck(A):
+    check = 0
+    for i in range(len(a)):
+        if A[i][i] == 2:
+            check += 1
+    if check == len(A):
+        print("all diagonal elements are two")
+    else: print("not all diagonal elements are two")
+
+diagonalCheck(a)
