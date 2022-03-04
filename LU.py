2a010c47e38bd446e4925562a532711a3cf42e3f
Lama, Christopher <lamac@stthom.edu>
2022-03-04 16:19:02 -0600

    added GENERATE(n) to MatrixGeneration.py

--- LU.py
+++ LU.py
@@ -92,25 +92,3 @@
 # FORWARD_E(True)
 # BACKWARD_E(True)   #correct results are 1.42857143,1.9047619 ,1.42857143
 
-def GENERATE(n):
-    #set up the Matrix
-    Matrix  = []
-    difference = n-1
-    for i in range(n):
-        Matrix.append([])
-        for j in range(n):
-            Matrix[i].append(0)
-
-    for j in range(0,n):#j=0
-        Matrix[j][j] = 2
-        for i in range(j+1,n):#n=5 --> 1-->5
-            Matrix[j][i] = -1
-            Matrix[i][j] = -1
-            break
-
-
-    print("A= \n")
-    for i in range(n):
-        print(Matrix[i])
-
-GENERATE(5)
