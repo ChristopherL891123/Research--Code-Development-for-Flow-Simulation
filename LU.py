24583115a8b56b7558c75c065f38c682b43dfc23
Lama, Christopher <lamac@stthom.edu>
2022-03-04 16:18:26 -0600

    added GENERATE(n) to MatrixGeneration.py

--- LU.py
+++ LU.py
@@ -95,14 +95,19 @@
 def GENERATE(n):
     #set up the Matrix
     Matrix  = []
+    difference = n-1
     for i in range(n):
         Matrix.append([])
         for j in range(n):
             Matrix[i].append(0)
 
-    for j in range(n):
-        for i in range(n):
-         Matrix[j][j] = 2
+    for j in range(0,n):#j=0
+        Matrix[j][j] = 2
+        for i in range(j+1,n):#n=5 --> 1-->5
+            Matrix[j][i] = -1
+            Matrix[i][j] = -1
+            break
+
 
     print("A= \n")
     for i in range(n):
