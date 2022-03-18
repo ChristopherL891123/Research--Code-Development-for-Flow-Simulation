b7a5c85e6c33eb4f134bf191955ff4f5188ecf3e
Lama, Christopher <lamac@stthom.edu>
2022-03-18 14:17:43 -0500

    added exception handling in main.py

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import GUI_RESEARCH
 """"Solves Ax = b """
 
 def main():
-    n = int(input("Size of matrix A to generate = "))
-    A = MatrixGeneration.GENERATE(n) #Generates a matrix called Matrix
-    print()
-    LU.SOLVE(A,n)
-
+    try:
+        n = int(input("Size of matrix A to generate = "))
+        A = MatrixGeneration.GENERATE(n) #Generates a matrix called Matrix
+        print()
+        LU.SOLVE(A,n)
+    except:
+        print("ERROR: Size of matrix A must be a positive integer.")
 main()
